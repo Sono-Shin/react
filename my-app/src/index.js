d4ceba450837994963755d953c3ff613d8366083
sh.shin <hoforyou@naver.com>
2025-01-16 11:47:24 +0900

    챕터 6 7 8 업로드

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import Library from './chapter_03/Library';
 //import Clock from './chapter_04/Clock';
-import CommentList from './chapter_05/CommentList';
+//import CommentList from './chapter_05/CommentList';
+//import NotificationList from './chapter_06/NotificationList';
+//import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
   root.render(
-      <CommentList />
+      <ConfirmButton />
     ,
     document.getElementById('root')
   );
